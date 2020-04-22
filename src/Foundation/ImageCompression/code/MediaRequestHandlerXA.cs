f6e2800046799eea6073aef10cf4aab7ca9ff819
Tom Tyack <thomastyack@aceik.com.au>
2020-04-22 13:42:49 +0000

    WIP

--- src/Foundation/ImageCompression/code/MediaRequestHandlerXA.cs
+++ src/Foundation/ImageCompression/code/MediaRequestHandlerXA.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using Sitecore.Pipelines;
 using Sitecore.Resources.Media;
-using Sitecore.Resources.Media.Streaming;
-using Sitecore.Web;
 using Sitecore.XA.Foundation.MediaRequestHandler.Pipelines.MediaRequestHandler;
 
 namespace Sitecore.Foundation.ImageCompression
@@ -29,24 +24,16 @@ namespace Sitecore.Foundation.ImageCompression
             Diagnostics.Assert.ArgumentNotNull(request, "request");
             Diagnostics.Assert.ArgumentNotNull(media, "media");
 
-            var chrome = context.Request.UserAgent.ToLower().Contains("chrome");
+            // But note that some browsers are lying: Chrome for example reports both as Chrome and Safari. So to detect Safari you have to check for the Safari string and the absence of the Chrome string, Chromium often reports itself as Chrome too or Seamonkey sometimes reports itself as Firefox.
             var safari = !context.Request.UserAgent.ToLower().Contains("chrome") && context.Request.UserAgent.ToLower().Contains("safari");
 
-            if (safari)
+            if (safari || context.Request.Browser.Type.ToUpper() == "IE")
             {
                 return base.DoProcessRequest(context, request, media);
             }
 
             if (request.InnerRequest.Path.ToLower().IndexOf(".webp") > -1)
             {
-                //MediaStream normalStreamFromCache = GetStreamFromCache(media, request.Options);
-                //if (normalStreamFromCache != null)
-                //{
-                //    ProcessMediaStreamCache(normalStreamFromCache, request, media, context);
-                //    return true;
-                //}
-
-
                 // So why use a link field here ... Sitecore has a limitation on the default attachement field called Blob
                 // You can only have one of these
                 // So unfortunately you can't sore the WEBP and the JPEG binary data on the one Media Item in Sitecore. 
@@ -56,47 +43,9 @@ namespace Sitecore.Foundation.ImageCompression
                 {
                     var myMedia = new Data.Items.MediaItem(relatedField.TargetItem);
                     media = new Media(new MediaData(myMedia));
-
-                    //MediaStream webpStreamFromCache = GetStreamFromCache(webpMedia, request.Options);
-                    //if (webpStreamFromCache != null)
-                    //{
-
-                    //}
-
-                    //MediaManager.Cache.AddStream(media, request.Options, webpMedia.GetStream(), out webpStreamFromCache);
-                    //ProcessMediaStreamCache(webpStreamFromCache, request, media, context);
-                    // return true;
                 }
             }
             return base.DoProcessRequest(context, request, media);
         }
-
-        //private void ProcessMediaStreamCache(MediaStream streamFromCache, MediaRequest request, Media media, HttpContext context)
-        //{
-        //    RaiseEvent("media:request", request);
-        //    if (Configuration.Settings.Media.EnableRangeRetrievalRequest && Configuration.Settings.Media.CachingEnabled)
-        //    {
-        //        using (streamFromCache)
-        //        {
-        //            SendMediaHeaders(media, context);
-        //            RangeRetrievalRequest request2 = RangeRetrievalRequest.BuildRequest(context, media);
-        //            RangeRetrievalResponse rangeRetrievalResponse = new RangeRetrievalResponse(request2, streamFromCache);
-        //            rangeRetrievalResponse.ExecuteRequest(context);
-        //            return;
-        //        }
-        //    }
-        //    SendMediaHeaders(media, context);
-        //    SendStreamHeaders(streamFromCache, context);
-        //    using (streamFromCache)
-        //    {
-        //        context.Response.AddHeader("Content-Length", streamFromCache.Stream.Length.ToString());
-        //        WebUtil.TransmitStream(streamFromCache.Stream, context.Response, Configuration.Settings.Media.StreamBufferSize);
-        //    }
-        //}
-
-        //private MediaStream GetStreamFromCache(Media media, MediaOptions options)
-        //{
-        //    return MediaManager.Cache.GetStream(media, options);
-        //}
     }
 }
